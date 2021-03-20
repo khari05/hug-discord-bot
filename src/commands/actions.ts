901847754d5ae4532ae8dfa5fa8b8e82c56e8eae
Karthik Hari <35894769+khari05@users.noreply.github.com>
2021-03-20 01:26:06 -0400

    hug more than one person

--- src/commands/actions.ts
+++ src/commands/actions.ts
@@ -23,41 +23,41 @@ interface Action {
 
 enum ActionType {
   singleUser,
-  dualUser
+  multiUser
 }
 
 export const actionMap: Map<string, Action> = new Map([
   ['hug', {
     command: { name: 'hug', description: 'Hug another user', cmd: action },
-    type: ActionType.dualUser,
+    type: ActionType.multiUser,
     phrase: (a, b) => `***${a} hugs ${b}***`,
     searchTerm: 'hug anime',
     links: []
   }],
   ['kiss', {
     command: { name: 'kiss', description: 'Kiss another user', cmd: action },
-    type: ActionType.dualUser,
+    type: ActionType.multiUser,
     phrase: (a, b) => `***${a} kisses ${b}***`,
     searchTerm: 'kiss anime',
     links: []
   }],
   ['headpat', {
     command: { name: 'headpat', description: 'Pat another user', cmd: action },
-    type: ActionType.dualUser,
+    type: ActionType.multiUser,
     phrase: (a, b) => `***${a} pats ${b}***`,
     searchTerm: 'head pat anime',
     links: []
   }],
   ['cuddle', {
     command: { name: 'cuddle', description: 'Cuddle with another user', cmd: action },
-    type: ActionType.dualUser,
+    type: ActionType.multiUser,
     phrase: (a, b) => `***${a} cuddles ${b}***`,
     searchTerm: 'snuggle anime',
     links: []
   }],
   ['holdhands', {
     command: { name: 'holdhands', description: 'Hold hands with another user', cmd: action },
-    type: ActionType.dualUser,
+    type: ActionType.multiUser,
     phrase: (a, b) => `***${a} holds hands with ${b}***`,
     searchTerm: 'holding hands anime',
     links: []
@@ -78,30 +78,34 @@ async function action (cmd: string, msg: Message): Promise<void> {
     await msg.channel.send('Sorry, that command is disabled in this channel.')
   } else {
     if (action !== undefined && action.links.length !== 0) {
-      let person2: string = ''
+      let person2: string = 'themself'
 
-      if (action.type === ActionType.dualUser) {
-        const mention = msg.content.match(/^.\w+\s<@!?(\d+)>/)
-        if (mention !== null && mention.length === 2) {
-          if (msg.author.id !== mention[1]) {
-            person2 = `<@${mention[1]}>`
+      if (action.type === ActionType.multiUser) {
+        const mention = msg.content.match(/<@!?\d+>+?/g)
+        if (mention !== null) {
+          const mentionIds: string[] = mention.map((a: string) => {
+            const match = a.match(/\d+/)
+            return match !== null ? match[0] : ''
+          })
+          if (mention.length === 1 && msg.author.id !== mentionIds[0]) {
+            person2 = `<@${mentionIds[0]}>`
+          } else if (mention.length === 2) {
+            person2 = `<@${mentionIds[0]}> and <@${mentionIds[1]}>`
           } else {
-            person2 = 'themself'
+            person2 = 'multiple people'
           }
         } else if (msg.mentions.users.size === 1) {
           person2 = `<@${msg.mentions.users.first()?.id ?? ''}>`
-        } else if (msg.content.match(/^.\w+\s(@everyone|@here)/) !== undefined) {
+        } else if (msg.content.match(/^.\w+\s(@everyone|@here)/) !== null) {
           person2 = 'the entire server'
         }
       }
 
-      if (person2.length !== 0 || action.type === ActionType.singleUser) {
-        await sendGif(
-          action.phrase(`<@${msg.author.id}>`, person2),
-          msg.channel,
-          action.links[Math.floor(Math.random() * (action.links.length - 1))]
-        )
-      }
+      await sendGif(
+        action.phrase(`<@${msg.author.id}>`, person2),
+        msg.channel,
+        action.links[Math.floor(Math.random() * (action.links.length - 1))]
+      )
     }
   }
 }
